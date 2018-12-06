a957d00eb236ea746b1730417777388d0a88bffe
Khaled M <k.code@icloud.com>
2018-12-06 04:05:05 -0500

    Improved styling & layour + made buttons erase message in title

--- colorgame.js
+++ colorgame.js
@@ -9,36 +9,41 @@ var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
-easyBtn.addEventListener("click", function() {
+//Easy Button function
+easyBtn.addEventListener("click", function () {
     hardBtn.classList.remove("selected");
     easyBtn.classList.add("selected");
     numSquares = 3;
     colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
-    for(var i=0; i<squares.length; i++) {
-        if(colors[i]) {
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
             squares[i].style.backgroundColor = colors[i];
         } else {
             squares[i].style.display = "none";
         }
     }
+    messageDisplay.textContent = "";
 });
 
-hardBtn.addEventListener("click", function() {
+//Hard Button function
+hardBtn.addEventListener("click", function () {
     easyBtn.classList.remove("selected");
     hardBtn.classList.add("selected");
     numSquares = 6;
     colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
-    for(var i=0; i<squares.length; i++) {
-            squares[i].style.backgroundColor = colors[i];
-            squares[i].style.display = "block";
-        }
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+    messageDisplay.textContent = "";
 });
 
-resetButton.addEventListener("click", function() {
+//Reset Button function
+resetButton.addEventListener("click", function () {
     //generate all new colors
     colors = generateRandomColors(numSquares);
     //pick a new random color from array
@@ -46,10 +51,12 @@ resetButton.addEventListener("click", function() {
     //change colorDisplay to match picked color
     colorDisplay.textContent = pickedColor;
     //change colors of squares on page
-    for(var i=0; i<squares.length; i++) {
+    for (var i = 0; i < squares.length; i++) {
         squares[i].style.background = colors[i];
     }
-    h1.classList.add("page-title");
+    h1.classList.add("pageTitle");
+    resetButton.textContent = "Reset";
+    messageDisplay.textContent = "";
 });
 
 colorDisplay.textContent = pickedColor;
