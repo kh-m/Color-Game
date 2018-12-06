113dd6f014438d68babe586700b7e20ea5dd9ace
Khaled M <k.code@icloud.com>
2018-12-06 02:36:59 -0500

    Added easy + hard buttons, with basic js structure

--- colorgame.js
+++ colorgame.js
@@ -6,6 +6,16 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+    alert("EASY BUTTON CLICKED");
+});
+
+hardBtn.addEventListener("click", function() {
+    alert("HARD BUTTON CLICKED");
+});
 
 resetButton.addEventListener("click", function() {
     //generate all new colors
